8a559da58a9dfb8d3f0a9c2a74bdf4a8cc81fc4c
Diego Silva <diegofns@gmail.com>
2015-10-27 20:49:04 -0200

    término aula 2

--- javascript/aula2/js/script.js
+++ javascript/aula2/js/script.js
@@ -1,9 +1,9 @@
-// alert("Bem vindo ao meu site");
-// alert("Essa é minha primeira aula de JS");
-// alert("Daqui a pouco serei um excelente programador");
-// prompt("Digite seu nome");
-// prompt("Digite a sua idade");
-// prompt("Digite a sua cidade");
+alert("Bem vindo ao meu site");
+alert("Essa é minha primeira aula de JS");
+alert("Daqui a pouco serei um excelente programador");
+prompt("Digite seu nome");
+prompt("Digite a sua idade");
+prompt("Digite a sua cidade");
 console.log(12 + 4);
 console.log("Diego Francisco");
 console.log(144 / 12 + 10);
