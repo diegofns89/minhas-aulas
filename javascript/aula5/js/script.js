e005bc78b0653e31909a4738c96aa0bb103d200b
Diego Silva <diegofns@gmail.com>
2015-11-10 18:30:45 -0200

    site relacionamento

---
+++ javascript/aula5/js/script.js
@@ -0,0 +1,20 @@
+if(25 >= 24){
+    console.log("Ok!");
+}else{
+    console.warn("Candidata não tem mais de 24 anos");
+}
+if ("Buritizeiro" == "Buritizeiro") {
+    console.log("Ok!");
+}else {
+  console.warn("A candidata não mora em Buritizeiro");
+}
+if ("musica" "pescaria" != "musica" "tiro ao alvo") {
+  console.log("Ok!");
+}else{
+  console.warn("A candidata não gosta de musica mas gosta de pescaria");
+}
+if (2 >= 0) {
+  console.log()
+}else {
+  console.warn("A candidata não tem filhos");
+}
---
+++ javascript/aula6/js/script.js
@@ -0,0 +1,20 @@
+if(25 >= 24){
+    console.log("Ok!");
+}else{
+    console.warn("Candidata não tem mais de 24 anos");
+}
+if("Buritizeiro" == "Buritizeiro"){
+  console.log("Ok!");
+}else{
+  console.warn("A candidata não mora em Buritizeiro");
+}
+if ("musica" "pescaria" == "musica" "pescaria") {
+  console.log("Ok!");
+}else{
+  console.warn("A candidata não gosta de musica e nem de pescaria");
+}
+if (0 = 0) {
+  console.log("Ok!");
+}else{
+  console.warn("A candidata tem filhos");
+}
