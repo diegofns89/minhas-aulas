f47e467e903d72e4a15f8003f114b967cd0881d0
Diego Silva <diegofns89@gmail.com>
2016-04-02 15:28:33 -0300

    close #12

---
+++ javascript/aula12/js/script.js
@@ -0,0 +1,8 @@
+var nome = prompt("Nome do usuário");
+var vezes = prompt("Quantas vezes ele quer repetir seu próprio nome");
+var indice = 0;
+
+while (indice < vezes) {
+  console.log(nome);
+  indice++;
+}
