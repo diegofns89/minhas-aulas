666af3f6d33ed03fb397f690b503f7a8c66d386f
Diego Silva <diegofns89@gmail.com>
2016-04-02 12:04:27 -0300

    Close #9

---
+++ javascript/aula7/js/script.js
@@ -0,0 +1,16 @@
+// Laço while executa enquanto a condição for verdadeira
+// Declarando variavel indice
+var indice = 1;
+// Console.log do while
+while (indice < 51) {
+  if (indice % 3 == 0)  {
+    console.log("pi");
+  }
+  else {
+    indice % 3 == 0
+  }
+  // Escreve no log do navegador
+  console.log(indice);
+  // Incrementar o índice
+  indice = indice + 1;
+}
---
+++ javascript/aula9/js/script.js
@@ -0,0 +1,8 @@
+var indice = 10;
+
+while (indice >= 1){
+  console.log("Contagem regressiva: " + indice + "...");
+  indice = indice - 1;
+
+}
+console.log("Fogo!!!");
