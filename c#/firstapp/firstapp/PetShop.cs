719a43a1add63456020e09990e8a126dfe779c25
Diego Silva <diegofns89@gmail.com>
2017-09-24 14:31:32 -0300

    'c#'

---
+++ c#/firstapp/firstapp/PetShop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstapp
+{
+    class PetShop
+    {
+        public string Name;
+        public string Address;
+        public string Description;
+        public bool Opened; public string Open()
+        {
+            Opened = true;
+            return "Open Now";
+        }
+        public string Close()
+        {
+
+            Opened = false;
+            return "Close. Back Tomorrow";
+        }
+    }
+}
+    
--- c#/firstapp/firstapp/Program.cs
+++ c#/firstapp/firstapp/Program.cs
@@ -6,32 +6,14 @@ namespace firstapp
     {
         static void Main(string[] args)
         {
-            Dog Jump = new Dog();
-            Jump.Color = "Branco";
-            Jump.Height = 1.5;
-            Jump.Mute = false;
-            Jump.Name = "Fred";
-            Jump.Breed = "2";
-            Jump.Gender = "Canis";
-            Jump.Age = 4;
-            Jump.Trained = true;
-            Console.WriteLine(Jump.Jump());
-
-            Dog Pricing = new Dog();
-            Pricing.Color = "Branco";
-            Pricing.Height = 1.5;
-            Pricing.Mute = false;
-            Pricing.Name = "Fred";
-            Pricing.Breed = "2";
-            Pricing.Gender = "Canis";
-            Pricing.Age = 4;
-            Pricing.Trained = true;
-            Pricing.Price = 2;
 
 
-            Console.WriteLine(Pricing.Pricing());
+            PetShop Open = new PetShop();
+            Console.WriteLine(Open.Open());
 
-
+            PetShop Close = new PetShop();
+            Close.Opened = false;
+            Console.WriteLine(Close.Close());
 
 
             Console.ReadLine();
